40e4912b234f8435199e24f8eecc6d6a39cdbf0c
Mike Nieva <m@mikenieva.com>
2023-11-30 16:22:58 -0600

    task 3

---
+++ task-03/index.js
@@ -0,0 +1,11 @@
+// A. DETERMINAR VARIABLES
+// 1. DETERMINAR EL TOTAL DE DISEÑOS
+const totalDesigns = 20
+
+// 2. DETERMINAR EL TOTAL DE DESARROLLADORES
+const totalDevs = 3
+
+// B. CÁLCULO DE DISEÑOS SIN TOCAR
+const notWorkedDesigns = totalDesigns % totalDevs
+
+console.log(notWorkedDesigns)
