ce5d69c527344809f3e603e37993db2fd6577cec
Mike Nieva <m@mikenieva.com>
2023-11-30 16:52:30 -0600

    notas adicionales

--- task-01/index.js
+++ task-01/index.js
@@ -1,2 +1,14 @@
 alert("hola mundo");
 console.log("Hola, mundo!")
+
+let persona = {
+    nombre: "Juan",
+    edad: 30,
+    profesion: "Programador",
+    saludar: function() {
+        console.log("Hola, mi nombre es " + this.nombre);
+    }
+};
+
+console.log(persona);
+console.dir(persona);
